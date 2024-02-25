96eddb15883ad4d7ff854dfb7dff498b5ca385ba
Tiiktak <dengsh268@outlook.com>
2024-02-25 19:27:21 +0800

    add args.trying_times to control the trying time sof the login loop

--- bocchi_link_terminal.py
+++ bocchi_link_terminal.py
@@ -7,13 +7,17 @@ def get_args():
     parser.add_argument('username', help='school card id')
     parser.add_argument('password', help='password of ehall.szu.edu.cn')
     parser.add_argument('-l', '--log-path', help='log file path', default='bocchi_link.log')
+    parser.add_argument('-tt', '--trying-times', help='loop trying times', default=-1, dtype=int)
     return parser.parse_args()
 
 
 def main():
     args = get_args()
-    bocchi_link = Bocchi(args.username, args.password, args.log_path)
-    bocchi_link.loop()
+    bocchi_link = Bocchi(args.username, args.password, args.trying_times, log_path=args.log_path)
+    if args.trying_times == -1:
+        bocchi_link.loop()
+    else:
+        bocchi_link.trying()
 
 
 if __name__ == '__main__':
